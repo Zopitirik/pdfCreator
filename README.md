43227f440de5a26e7858f5504309009af6ab4395
Hikmet Ergun <hikmet.ergun@erkaturizm.com>
2019-10-30 14:29:10 +0300

    output directory

--- README.md
+++ README.md
@@ -8,17 +8,16 @@ npm i pdfcreator
 
 ## Usage
 ```
-pdfcreator -u [url] -o [fileName]
+pdfcreator -u [url] -o [fileName] -d [directoryName]
 ```
 
 ```url``` http/https valid
 
 ```fileName``` should be just a name without extension
 
+```directory``` directory Ex: C:\Pdf.  If directory does not exist, it will be created. (Optional)
+
 ## Test
 ```
 pdfcreator -u https://github.com/ -o github
 ```
-
-
-
--- pdfCreator.js
+++ pdfCreator.js
@@ -3,26 +3,32 @@ const colors = require('colors')
 const program = require('commander')
 const puppeteer = require('puppeteer')
 const path = require('path')
+const fs = require("fs")
 
 //Program Inputs
 program
     .version('0.0.1')
     .usage('-u [url] -o [fileName]')
-    .option('-u, --url [path]', 'Url for pdf')
-    .option('-o, --outputFileName [filename]', 'Output file name [fileName].pdf (without ext)')
+    .option('-u, --url [path]', 'url for pdf')
+    .option('-o, --outputFileName [filename]', 'output file name [fileName].pdf (without ext)')
+    .option('-d, --outputDirectory [directory]', 'output directory path.\n\t\t\t\texample: C:\\foo\\bar\n\t\t\t\tif not provided, current directory will be used.\n\t\t\t\tif directory not exist, it will be created.')
 
 program.parse(process.argv)
 
-if (!program.outputFileName | !program.url) {
-    consoleError('Please specifiy an input file & a url');
+if (!program.url) {
+    consoleError('Please specifiy a url.\nFor help : pdfCreator -h')
+}
+
+if (program.outputDirectory && !fs.existsSync(program.outputDirectory))  {
+    fs.mkdirSync(program.outputDirectory);
 }
 
 //Main
 async function main() {
     console.log('Started!'.green)
     var taskPromise = await urlToPdf(program.url)
-    if (taskPromise == 'OK') {
-        console.log('Finished!'.green)
+    if (taskPromise) {
+        console.log(taskPromise.green)
         process.exit(1)
     }
     else
@@ -32,19 +38,30 @@ async function main() {
 }
 
 function consoleError(msg) {
-    console.error(msg.red);
+    console.error(msg.red)
     process.exit(1)
 }
 
 //Core Function
 async function urlToPdf(url) {
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage()
-    const inputDir = path.resolve(__dirname, '.')
-    const outputPath = path.resolve(path.join(inputDir, program.outputFileName + '.pdf'))
-    await page.goto(url, { waitUntil: 'networkidle2' });
-    await page.pdf({ path: outputPath, printBackground: true })
-    return 'OK'
+    try {
+        const browser = await puppeteer.launch({ headless: true })
+        const page = await browser.newPage()
+        var _inputDir = __dirname
+        var fileName = new Date().getTime()
+
+        if (program.outputDirectory) _inputDir = program.outputDirectory
+        if (!program.outputFileName) fileName = program.outputFileName
+        
+        const inputDir = path.resolve(_inputDir, '.')
+        const outputPath = path.resolve(path.join(inputDir, fileName + '.pdf'))
+        
+        await page.goto(url, { waitUntil: 'networkidle2' })
+        await page.pdf({ path: outputPath, printBackground: true })
+        return outputPath
+    } catch (error) {
+        consoleError(error)
+    }
 }
 
 //Run
